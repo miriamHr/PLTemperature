9dd4d9c43685c5af019f68e773f58c4a830753bf
miriam <alu0100603811@ull.edu.es>
2013-02-14 20:02:46 -0100

    expresion

--- temperature.js
+++ temperature.js
@@ -1,23 +1,23 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 function calculate() {
   var result;
-  var original       = document.getElementById("........");
+  var original = document.getElementById("original");
   var temp = original.value;
-  var regexp = /.............................../;
+  var regexp = /-?[0-9]*\.?[0-9]*[a-zA-Z]+/;
   
-  var m = temp.match(......);
+  var m = temp.match(regexp);
   
   if (m) {
-    var num = ....;
-    var type = ....;
+    var num = m.substring(m.length-1,0);
+    var type = m.substring(1, m.length-1);
     num = parseFloat(num);
     if (type == 'c' || type == 'C') {
       result = (num * 9/5)+32;
-      result = ..............................
+      result = 'C';
     }
     else {
       result = (num - 32)*5/9;
-      result = ............................
+      result ='F';
     }
     converted.innerHTML = result;
   }
